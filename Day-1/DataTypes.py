5e818109b31ac76bb7e27c452c10e625a2a75026
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-28 17:04:46 +0500

    commit

---
+++ Day-1/DataTypes.py
@@ -0,0 +1,24 @@
+# python data types 
+# python built-in data types are following 
+
+
+# int  => integer data type
+# str => string or text data type 
+# list,tuple ,range => sequence data type
+# bool => boolean data type
+# NoneTYPE => NONEdata type
+# bytearray, bytes ,memoryview => binary data types 
+
+# python numbers
+"""_
+float
+integer
+complex 
+"""
+
+x=5
+y=2.5
+z=4j
+print(type(x ))  #int
+print(type(y ))   #float
+print(type(z ))   #complex   
