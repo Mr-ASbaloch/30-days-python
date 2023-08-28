9c16a7cdfb332b23b9cecdaa12a45ac4ecba8381
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-08-28 16:02:11 +0500

    commit

---
+++ Day-1/intro.py
@@ -0,0 +1,31 @@
+print ("hello word !")
+print ("this is first word")
+
+
+
+
+# Indentation in python 
+# Python uses indentation to indicate a block of code.
+
+if 5 > 3 :
+# Python will give you an error if you skip the indentation:
+
+# print ("number is greater than 2 ") # it will show error because of indentation block : indented block is required
+
+ print("number is greater than 3")  # it will run correctly because it has indented block 
+ 
+#  number of spaces are upto you as a programmmer but at least one is required 
+
+if 5 > 4:
+ print("4 is smaller than 5")
+ 
+ 
+ if 5 > 4:
+          print("4 is smaller than 5")
+
+
+
+
+
+ 
+ 
